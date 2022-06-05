53c7404b2bde871f7b11c0ee5fbd5fa53d7be172
mangbaam <pmb0836@gmail.com>
2022-06-05 11:52:53 +0900

    add empty thumbnail placeholder

--- app/src/main/java/mangbaam/practice/parkingfree/CampingRemoteDataSource.kt
+++ app/src/main/java/mangbaam/practice/parkingfree/CampingRemoteDataSource.kt
@@ -1,22 +1,16 @@
 package mangbaam.practice.parkingfree
 
-import android.util.Log
 import mangbaam.practice.parkingfree.dto.CampingResponse
 import mangbaam.practice.parkingfree.network.Network
 import mangbaam.practice.parkingfree.util.Api
 import mangbaam.practice.parkingfree.util.Constants.ANDROID
 import mangbaam.practice.parkingfree.util.Constants.APP_NAME
 import mangbaam.practice.parkingfree.util.Constants.KEY_NUMS_OF_ROWS
-import mangbaam.practice.parkingfree.util.Constants.TAG
 
 class CampingRemoteDataSource(private val apiService: Network) {
-    suspend fun getBasedCampingList(page: Int? = 1): CampingResponse {
-        val ret =apiService.getBaseList(
-            page, KEY_NUMS_OF_ROWS, ANDROID, APP_NAME, Api.KEY, "json"
-        )
-        Log.d(TAG, "CampingRemoteDataSource - getBasedCampingList() called / $ret")
-        return ret
-    }
+    suspend fun getBasedCampingList(page: Int? = 1) = apiService.getBaseList(
+        page, KEY_NUMS_OF_ROWS, ANDROID, APP_NAME, Api.KEY, "json"
+    )
 
     suspend fun getSearchedCampingList(page: Int? = 1, keyword: String): CampingResponse {
         return apiService.getSearchList(
